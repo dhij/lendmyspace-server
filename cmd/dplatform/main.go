9fa6680b500371e82ef5ad653af8d175fd729163
David Hwang <davidhwang.ij@gmail.com>
2021-12-26 00:50:52 -0500

    Initialize db

--- cmd/dplatform/main.go
+++ cmd/dplatform/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"github.com/davidhwang-ij/study-platform/routers"
+	"dplatform/db"
+	"dplatform/routers"
+	"log"
 )
 
 func main() {
 	r := routers.InitRouter()
+	dbSQLX, err := db.NewDatabase()
+	if err != nil {
+		log.Fatalf("Could not initialize Database connection using sqlx %s", err)
+	}
+	defer dbSQLX.Close()
+
 	r.Run(":8080")
 }
