e04beb9fcf044c43181b16dcf159c5a79fa489b4
David Hwang <davidhwang.ij@gmail.com>
2021-12-26 00:52:01 -0500

    make db connection

---
+++ db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+const (
+	maxIdleConns = 10
+	maxOpenConns = 10
+	maxConnLife  = 5 * time.Minute
+)
+
+type PostgreSQLsqlx struct {
+	db *sqlx.DB
+}
+
+// NewDatabase make new database connection
+func NewDatabase() (*PostgreSQLsqlx, error) {
+	connectionStr := "postgres://root:password@dplatform_postgres:5432?sslmode=disable"
+	db, err := sqlx.Open("postgres", connectionStr)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetConnMaxLifetime(maxConnLife)
+
+	return &PostgreSQLsqlx{
+		db: db,
+	}, nil
+}
+
+func (p *PostgreSQLsqlx) Close() {
+	p.db.Close()
+}
