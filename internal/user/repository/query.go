10db0fd2c33a1abab405fdd2f9c413e5d4af7543
David Hwang <davidhwang.ij@gmail.com>
2021-12-30 17:51:37 -0500

    add getUser and listUsers in user repository

--- internal/user/repository/query.go
+++ internal/user/repository/query.go
@@ -1,6 +1,7 @@
 package repository
 
 const (
-	FindByIDQuery   = `SELECT user_name FROM users WHERE id = $1`
+	GetUserQuery    = `SELECT user_name FROM users WHERE id = $1 LIMIT 1`
+	ListUsersQuery  = `SELECT user_name, first_name, last_name, email FROM users ORDER BY id LIMIT $1 OFFSET $2`
 	CreateUserQuery = `INSERT INTO users(user_name, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING id`
 )
