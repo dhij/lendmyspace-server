ce904228af9e9a29f3e55d941c08bf449381ecc4
David Hwang <davidhwang.ij@gmail.com>
2022-01-01 12:36:15 -0500

    add random int and string generator

---
+++ util/random.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var userName string
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomUser() string {
+	userName = RandomString(6)
+	return userName
+}
+
+func RandomEmail() string {
+	return userName + "@gmail.com"
+}
+
+func RandomFirstName() string {
+	return RandomString(6)
+}
+
+func RandomLastName() string {
+	return RandomString(6)
+}
