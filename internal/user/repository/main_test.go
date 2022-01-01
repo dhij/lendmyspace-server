f57d95f4f1702a4e44e4c323def41d396cbe6425
David Hwang <davidhwang.ij@gmail.com>
2022-01-01 11:12:02 -0500

    add main test for db connection

---
+++ internal/user/repository/main_test.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"dplatform/db"
+	"log"
+	"os"
+	"testing"
+)
+
+var dbSQLX *db.PostgreSQL
+
+func TestMain(m *testing.M) {
+	var err error
+	dbSQLX, err = db.NewDatabase()
+	if err != nil {
+		log.Fatalf("Could not initialize Database connection using sqlx %s", err)
+	}
+
+	os.Exit(m.Run())
+}
