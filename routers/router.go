2150014c446d45a3504dfe34755abeef1c2c32be
David Hwang <davidhwang.ij@gmail.com>
2022-01-18 20:09:59 -0500

    add cors to the router

--- routers/router.go
+++ routers/router.go
@@ -3,7 +3,9 @@ package routers
 import (
 	http2 "lendmyspace-server/internal/space/http"
 	"lendmyspace-server/internal/user/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,18 @@ var r *gin.Engine
 
 func InitRouter(userHandler *http.UserHandler, spaceHandler *http2.SpaceHandler) {
 	r = gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	r.GET("/users/:user_id", userHandler.GetUser)
 	r.GET("/users", userHandler.ListUsers)
