e77e7f84eab7bf727ce91bf66efaad1373afcf87
David Hwang <davidhwang.ij@gmail.com>
2022-01-20 20:41:03 -0500

    add query and repository to list spaces

--- internal/space/repository/query.go
+++ internal/space/repository/query.go
@@ -2,5 +2,6 @@ package repository
 
 const (
 	GetSpaceQuery    = `SELECT * FROM spaces WHERE id = $1 LIMIT 1`
+	ListSpacesQuery  = `SELECT id, name, description, location, link FROM spaces ORDER BY id`
 	CreateSpaceQuery = `INSERT INTO spaces (name, description, location, link, host_id, image_id, date_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
 )
